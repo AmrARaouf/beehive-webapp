9897820e5c427a63006063399d8dded9418c23be
Amr Abdelraouf <ga47wuj@mytum.de>
2017-06-18 19:20:47 +0200

    add business sevice signup

--- src/app/services/business.service.ts
+++ src/app/services/business.service.ts
@@ -1,8 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
+
+import { environment } from './../../environments/environment';
+import { BusinessUser } from './../models'
 
 @Injectable()
 export class BusinessService {
 
-  constructor() { }
+  private headers = new Headers({'Content-Type': 'application/json'});
+  private baseUrl = environment.apiUrl;
+
+  constructor(private http: Http) {
+
+  }
+
+  signup(businessUser: BusinessUser): Promise<BusinessUser> {
+    return this.http.post(`${this.baseUrl}business/users`, JSON.stringify(businessUser))
+          .toPromise()
+          .then( response => response.json().data as BusinessUser);
+  }
 
 }
