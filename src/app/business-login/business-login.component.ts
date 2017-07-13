c6048658ce0a161d97fd0935bf930785323aa0c2
Amr Abdelraouf <ga47wuj@mytum.de>
2017-07-13 18:31:26 +0200

    remove routers from services

--- src/app/business-login/business-login.component.ts
+++ src/app/business-login/business-login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { BusinessService } from '@app/_services/business.service';
 import { BusinessUserCredentials } from '@app/models';
@@ -14,9 +15,12 @@ export class BusinessLoginComponent {
     password: ''
   }
 
-  constructor(private businessService: BusinessService) { }
+  constructor(private businessService: BusinessService,
+              private router: Router) { }
 
   login(): void {
-    this.businessService.login(this.userCredentials).then();
+    this.businessService.login(this.userCredentials).then( user => {
+      this.router.navigate(['/projects'])
+    });
   }
 }
