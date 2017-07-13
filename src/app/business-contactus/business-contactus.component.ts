2962bb67069f69531123e71312ac7a2739b9bcb7
Salma <salmagalal94@gmail.com>
2017-07-13 13:58:26 +0200

    Changed signup to contact us

---
+++ src/app/business-contactus/business-contactus.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+import { BusinessUser } from '@app/models';
+import { BusinessService } from '@app/_services/business.service';
+
+@Component({
+  selector: 'app-business-contactus',
+  templateUrl: './business-contactus.component.html',
+  styles: []
+})
+export class BusinessContactUsComponent {
+  user: BusinessUser = {
+    full_name: '',
+    organization: '',
+    email: '',
+    phone: '',
+    password: '',
+    message: ''
+  };
+
+  constructor(private businessService: BusinessService) { }
+
+  signup(): void {
+    this.businessService.signup(this.user).then();
+  }
+}
