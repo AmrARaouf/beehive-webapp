7856615b57c8ac59c6843270d90268e5ee523ee5
Faisal Hafeez <fhnaseer@live.com>
2017-07-17 21:10:29 +0200

    Adding labels tab,

--- src/app/project-detail/project-detail.component.ts
+++ src/app/project-detail/project-detail.component.ts
@@ -3,7 +3,7 @@ import { Router, ActivatedRoute, Params } from '@angular/router';
 
 import { ProjectService } from '@app/_services/project.service';
 
-import { Project, Activity } from '@app/models';
+import { Project, Activity, Annotation } from '@app/models';
 
 @Component({
     selector: 'app-project-detail',
@@ -27,23 +27,41 @@ export class ProjectDetailComponent {
                 this.project = project;
                 // console.log(this.project);
                 // Populate data for charts,
-                this.imageLabels = Array(0);
-                this.imageAnnotationCount = Array(0);
-                var notLabelled = 0;
-                this.project.images.forEach((item, index) => {
-                    if (item.annotations.length == 0)
-                        notLabelled++;
-                    else {
-                        this.imageLabels.push('Image ' + (index+1).toString());
-                        this.imageAnnotationCount.push(item.annotations.length);
-                    }
-                });
-                this.imageLabels.push('Not Labelled');
-                this.imageAnnotationCount.push(notLabelled);
+                this.populateChartsData();
                 this.projectService.getProjectActivities(this.project._id).then(activities => {
                     this.activities = Array(0);
                     activities.forEach((item, index) => this.activities.push(<ActivityModel> { id:index+1, activity: item}));
                 });
+            });
+        });
+    }
+
+    populateChartsData() : void {
+        this.labels = this.project.labelNames;
+        this.labelCount = Array(0);
+        this.imageLabels = Array(0);
+        this.imageAnnotationCount = Array(0);
+        var notLabelled = 0;
+        this.project.images.forEach((item, index) => {
+            if (item.annotations.length == 0)
+                notLabelled++;
+            else {
+                this.imageLabels.push('Image ' + (index+1).toString());
+                this.imageAnnotationCount.push(item.annotations.length);
+                this.populateLabelsData(item.annotations);
+            }
+        });
+        this.imageLabels.push('Not Labelled');
+        this.imageAnnotationCount.push(notLabelled);
+    }
+
+    populateLabelsData(annotations : Annotation[]) : void {
+        annotations.forEach((annotation, index) => {
+            if (annotation.labels == null) return;
+            annotation.labels.forEach((item, index) => {
+            var ind = this.labels.findIndex(x => x== item.name);
+                if (ind != -1)
+                    this.labelCount[index] = this.labelCount[index]+1;
             });
         });
     }
